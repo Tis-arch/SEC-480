7d89c3ee2b8df1e574e00d58d6fabd53ae0cfe15
Tis-arch <john.tiseo@mymail.champlain.edu>
2023-03-22 18:58:43 -0400

    End of M6 Finally

--- 480modscripts/480driver.ps1
+++ 480modscripts/480driver.ps1
@@ -17,8 +17,21 @@ Set-Network -vServer $conf.vcenter_server
 VMBoot
 
 linkedCloner
-#>
 
+Write-Host "Virtual Switch and Portgroup creator active. Stop process to exit."
+$vSwitchName = Read-Host -Prompt "Enter the name for the new Virtual Switch"
+$pGroupName = Read-Host -Prompt "Enter the name for the new Portgroup"
+vSwitch -vSwitchName $vSwitchName -pGroupName $pGroupName -vHost $conf.vm_host -vServer $conf.vcenter_server
+Get-VirtualSwitch
+Get-VirtualPortGroup
+#Write-Host "VM Identifier active. Stop process to exit."
 Get-VMInfo -vServer $conf.vcenter_server
+#>
+Write-Host "VM Cloner active. Stop process to exit."
+#linkedCloner
+Write-Host "VM Boot active. Stop process to exit."
+VMBoot
+Write-Host "Network Setter active. Stop process to exit."
+Set-Network -vServer $conf.vcenter_server
 
 
